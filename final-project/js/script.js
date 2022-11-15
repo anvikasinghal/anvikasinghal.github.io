cfec7d4de8ae77eb79b9ce8f632054857f6d6cf2
anvikasinghal <asinghal@eastsideprep.org>
2022-11-15 13:21:08 -0800

    final project update

--- final-project/js/script.js
+++ final-project/js/script.js
@@ -6,13 +6,17 @@ function contact() {
     let contactParagraph = document.getElementById("contact");
 
     // get values from form
-    fname = document.getElementById("fname").value;
+    let fname = document.getElementById("fname").value;
     let lname = document.getElementById("lname").value;
     let subject = document.getElementById("subject").value;
     let message = document.getElementById("message").value;
 
     // show results
-    contactParagraph.innerHTML = "thank you, " + fname + ", for your response!";
+    if (fname != "") {
+        contactParagraph.innerHTML = "thank you, " + fname + ", for your response!";
+    } else {
+        contactParagraph.innerHTML = "<i>FIRST NAME IS REQUIRED</i>"
+    }
 
     document.getElementById("contact-form").reset();
 }
